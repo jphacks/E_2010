ee9d55d559bdc241af4b273ea0c47dc60107db87
綾田陽介 <202x102x@stu.kobe-u.ac.jp>
2020-11-02 15:47:51 +0900

    settings.py を修正してMySQLに対応

--- lunch/lunch/settings.py
+++ lunch/lunch/settings.py
@@ -81,8 +81,11 @@ WSGI_APPLICATION = 'lunch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': 'pass',
     }
 }
 
